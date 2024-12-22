2bf53d956b51d7c0d6042dac351cda3eddb4f015
Error <110461609+errorfiathck@users.noreply.github.com>
2024-12-22 23:17:26 +0330

    Update traffic-sniffer.py

--- traffic-sniffer.py
+++ traffic-sniffer.py
@@ -1,144 +1,417 @@
+import ipaddress
 import os
-import time
 import socket
+import struct
+import sys
 import argparse
-from platform import node, system, release
-from twisted.internet import error
-from twisted.internet import reactor
-from twisted.internet import protocol
-from twisted.protocols.portforward import ProxyClient
-from twisted.protocols.portforward import ProxyFactory
-from twisted.protocols.portforward import ProxyClientFactory
-
-Node, System, Release = node(), system(), release()
-
-os.system("clear" if os.name == "posix" else "cls")
-
-def baner():
-    print(
-        """
- _______             ___    ___ _                       _    ___    ___             
-(_______)           / __)  / __|_)                     (_)  / __)  / __)            
-    _  ____ _____ _| |__ _| |__ _  ____ _____ ___ ____  _ _| |__ _| |__ _____  ____ 
-   | |/ ___|____ (_   __|_   __) |/ ___|_____)___)  _ \| (_   __|_   __) ___ |/ ___)
-   | | |   / ___ | | |    | |  | ( (___     |___ | | | | | | |    | |  | ____| |    
-   |_|_|   \_____| |_|    |_|  |_|\____)    (___/|_| |_|_| |_|    |_|  |_____)_|    
-                                                                        
-                                    by: errorfiathck
-"""
-    )
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QTableWidget,
+    QTableWidgetItem,
+    QVBoxLayout,
+    QWidget,
+    QLineEdit,
+    QPushButton,
+    QHBoxLayout,
+    QLabel,
+    QMessageBox,
+    QDialog,
+    QTextEdit,
+)
+import binascii  # For binary and hex conversion
 
 
+class IP:
+    def __init__(self, buff=None):
+        header = struct.unpack("<BBHHHBBH4s4s", buff)
+        self.ver = header[0] >> 4
+        self.ihl = header[0] & 0xF
 
-class Proxy(protocol.Protocol):
-    noisy = True
-    peer = None
-    intercept = True
+        self.tos = header[1]
+        self.len = header[2]
+        self.id = header[3]
+        self.offset = header[4]
+        self.ttl = header[5]
+        self.protocol_num = header[6]
+        self.sum = header[7]
+        self.src = header[8]
+        self.dst = header[9]
 
-    def setPeer(self, peer):
-        self.peer = peer
+        # human readable IP addresses
+        self.src_address = ipaddress.ip_address(self.src)
+        self.dst_address = ipaddress.ip_address(self.dst)
 
-    def connectionLost(self, reason):
-        if self.peer is not None:
-            self.peer.transport.loseConnection()
-            self.peer = None
-        elif self.noisy:
-            print("Unable to connect to peer: %s" % (reason,))
-
-    def dataReceived(self, data):
-        self.peer.transport.write(data)
-        # SSL/TLS handshake
-        if self.intercept and data.startswith("\x16\x03"):
-            self.intercept = False
-            print(
-                "\033[91m[!] %s: Disabling interception of SSL traffic: " \
-                "%s:%d\033[0m" % (
-                    time.strftime("%H:%M:%S"),
-                    self.peer.transport.getPeer().host,
-                    self.peer.transport.getPeer().port
-                )
-            )
-        if self.intercept:
-            print(data)
+        # map protocol constant to their names
+        self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
+        try:
+            self.protocol = self.protocol_map[self.protocol_num]
+        except Exception as e:
+            print("%s No protocol for %s" % (e, self.protocol_num))
+            self.protocol = str(self.protocol_num)
 
 
-class ProxyServer(Proxy):
-    clientProtocolFactory = ProxyClientFactory
-    reactor = None
-
-    def connectionMade(self):
-        self.transport.pauseProducing()
-        # TODO RTFM and tidy this ugly hack to determine port
-        target_port = int(str(self.transport).split("on ")[1].rstrip()[:-1])
-
-        client = self.clientProtocolFactory()
-        client.setServer(self)
-
-        if self.reactor is None:
-            self.reactor = reactor
-
-        print(
-            "\033[94m[+] %s: %s connected to TCP %s:%d->%s:%d\033[0m" % (
-                time.strftime("%H:%M:%S"),
-                self.transport.getHost().host,
-                self.transport.getPeer().host,
-                self.transport.getPeer().port,
-                self.factory.host,
-                target_port
-            )
-        )
-        self.reactor.connectTCP(self.factory.host, target_port, client)
+class ICMP:
+    def __init__(self, buff):
+        header = struct.unpack("<BBH", buff[:4])
+        self.type = header[0]
+        self.code = header[1]
+        self.checksum = header[2]
+        self.data = buff[4:]
 
 
-class MitmFactory(protocol.Factory):
-    protocol = ProxyServer
+def sniff(host, callback):
+    if os.name == "nt":
+        socket_protocol = socket.IPPROTO_IP
+    else:
+        socket_protocol = socket.IPPROTO_ICMP
 
-    def __init__(self, host):
-        self.host = host
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+    sniffer.bind((host, 0))
 
+    # include the IP header in the capture
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
-def main():
-    baner()
-
-    desc = "MITM plaintext comms"
-    parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument("--ip", "-i", type=str, required=True, help="victim server")
-    parser.add_argument("--port", "-p", type=str, required=True,
-                        help="single victim port or comma separated victim ports")
-    args = parser.parse_args()
+    if os.name == "nt":
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 
     try:
-        socket.inet_pton(socket.AF_INET, args.ip)
-    except AttributeError:
-        try:
-            socket.inet_aton(args.ip)
-        except socket.error:
-            print("[!] Invalid IPv4")
+        while True:
+            # read packets
+            raw_buffer = sniffer.recvfrom(65535)[0]
+            # create an IP header from the first 20 bytes
+            ip_header = IP(raw_buffer[0:20])
+
+            icmp_type = "N/A"
+            icmp_code = "N/A"
+            icmp_seq = "N/A"
+
+            if ip_header.protocol == "ICMP":
+                icmp_header = ICMP(raw_buffer[20:28])
+                icmp_type = icmp_header.type
+                icmp_code = icmp_header.code
+
+                # Extract the ICMP sequence number (if present)
+                if len(raw_buffer) > 28:
+                    icmp_seq = struct.unpack("!H", raw_buffer[28:30])[0]
+
+            packet_length = len(raw_buffer)
+
+            # Pass the information to the callback
+            callback(
+                ip_header.protocol,
+                ip_header.src_address,
+                ip_header.dst_address,
+                icmp_type,
+                icmp_code,
+                icmp_seq,
+                ip_header.ttl,
+                ip_header.id,
+                packet_length,
+                raw_buffer,  # Pass the raw_buffer here
+            )
+
+    except KeyboardInterrupt:
+        # if were on Windows, turn off promiscuous mode
+        if os.name == "nt":
+            sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+        sys.exit()
+
+
+class PacketDetailsWindow(QDialog):
+    def __init__(
+        self, protocol, src, dst, icmp_type, icmp_code, ttl, packet_id, raw_buffer
+    ):
+        super().__init__()
+        self.setWindowTitle("Packet Details")
+        self.setGeometry(200, 200, 600, 400)
+
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+
+        # Connection information
+        self.info_label = QLabel(
+            f"""
+        <b>Connection Details:</b><br>
+        Protocol: {protocol}<br>
+        Source: {src}<br>
+        Destination: {dst}<br>
+        ICMP Type: {icmp_type}<br>
+        ICMP Code: {icmp_code}<br>
+        TTL: {ttl}<br>
+        Packet ID: {packet_id}
+        """
+        )
+        self.layout.addWidget(self.info_label)
+
+        # Binary and Hex views
+        self.binary_view = QTextEdit()
+        self.binary_view.setReadOnly(True)
+        self.binary_view.setLineWrapMode(QTextEdit.NoWrap)
+
+        self.hex_view = QTextEdit()
+        self.hex_view.setReadOnly(True)
+        self.hex_view.setLineWrapMode(QTextEdit.NoWrap)
+
+        self.binary_view.setText(self.format_binary(raw_buffer))
+        self.hex_view.setText(self.format_hex(raw_buffer))
+
+        self.layout.addWidget(QLabel("<b>Binary Data:</b>"))
+        self.layout.addWidget(self.binary_view)
+
+        self.layout.addWidget(QLabel("<b>Hex Data:</b>"))
+        self.layout.addWidget(self.hex_view)
+
+    def format_binary(self, data):
+        """Converts raw data into a binary string representation."""
+        binary_data = " ".join(format(byte, "08b") for byte in data)
+        return binary_data
+
+    def format_hex(self, data):
+        """Converts raw data into a hex string representation."""
+        hex_data = binascii.hexlify(data).decode("utf-8")
+        return " ".join(hex_data[i : i + 2] for i in range(0, len(hex_data), 2))
+
+
+from PyQt5.QtWidgets import QCheckBox, QTabWidget
+
+
+class PacketSnifferApp(QMainWindow):
+    def __init__(self, default_host=""):
+        super().__init__()
+
+        self.setWindowTitle("Packet Sniffer")
+        self.setGeometry(100, 100, 1000, 600)
+
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+
+        self.layout = QVBoxLayout()
+        self.central_widget.setLayout(self.layout)
+
+        # Input for host
+        self.host_input_layout = QHBoxLayout()
+        self.host_label = QLabel("Host:")
+        self.host_input = QLineEdit()
+        self.host_input.setText(default_host)
+        self.start_button = QPushButton("Start Sniffing")
+        self.start_button.clicked.connect(self.start_sniffing)
+        self.stop_button = QPushButton("Stop Sniffing")
+        self.stop_button.clicked.connect(self.stop_sniffing)
+        self.host_input_layout.addWidget(self.host_label)
+        self.host_input_layout.addWidget(self.host_input)
+        self.host_input_layout.addWidget(self.start_button)
+        self.host_input_layout.addWidget(self.stop_button)
+        self.layout.addLayout(self.host_input_layout)
+
+        # Tabs for display management
+        self.tab_widget = QTabWidget()
+        self.layout.addWidget(self.tab_widget)
+
+        # Packet table tab
+        self.packet_table_tab = QWidget()
+        self.packet_table_layout = QVBoxLayout()
+        self.packet_table_tab.setLayout(self.packet_table_layout)
+        self.tab_widget.addTab(self.packet_table_tab, "Packets")
+
+        # Table for packet display
+        self.table = QTableWidget()
+        self.table.setColumnCount(9)
+        self.table.setHorizontalHeaderLabels(
+            [
+                "Protocol",
+                "Source",
+                "Destination",
+                "ICMP Type",
+                "ICMP Code",
+                "ICMP Seq",
+                "TTL",
+                "ID",
+                "Length",
+            ]
+        )
+        self.table.cellDoubleClicked.connect(self.show_packet_details)
+        self.packet_table_layout.addWidget(self.table)
+
+        # View options tab
+        self.view_options_tab = QWidget()
+        self.view_options_layout = QVBoxLayout()
+        self.view_options_tab.setLayout(self.view_options_layout)
+        self.tab_widget.addTab(self.view_options_tab, "View Options")
+
+        # Checkboxes for toggling column visibility
+        self.column_checkboxes = []
+        for col in range(self.table.columnCount()):
+            label = self.table.horizontalHeaderItem(col).text()
+            checkbox = QCheckBox(label)
+            checkbox.setChecked(True)
+            checkbox.stateChanged.connect(lambda state, col=col: self.toggle_column(col, state))
+            self.column_checkboxes.append(checkbox)
+            self.view_options_layout.addWidget(checkbox)
+
+
+        # Store raw packet data for detailed view
+        self.packet_data = []
+        self.sniffing_thread = None
+        self.sniffing_active = False
+
+    def start_sniffing(self):
+        if self.sniffing_active:
+            QMessageBox.warning(self, "Warning", "Sniffing is already running.")
             return
-    except socket.error:
-        print("[!] Invalid IPv4")
-        return
 
-    ports = args.port.split(",")
-    factory = MitmFactory(args.ip)
-    print("\033[1mZerodays burn \033[91mred\033[0m,\033[0m")
-    print("\033[1mSysadmins turn \033[94mblue\033[0m,\033[0m")
-    print("\033[1mReading logs is \033[91mhard\033[0m,\033[0m")
-    print("\033[1mWhy would \033[94myou\033[0m?\033[0m\n\n")
+        host = self.host_input.text()
+        if not host:
+            QMessageBox.warning(self, "Error", "Please enter a valid host IP address.")
+            return
 
-    for port in ports:
-        port = int(port)
-        try:
-            reactor.listenTCP(port, factory)
-        except error.CannotListenError:
-            print("\033[91m[!] %s: TCP 0.0.0.0:%d->%s:%d\033[0m" %
-                (time.strftime("%H:%M:%S"), port, args.ip, port)
+        self.sniffing_active = True
+
+        def callback(
+            protocol,
+            src,
+            dst,
+            icmp_type,
+            icmp_code,
+            icmp_seq,
+            ttl,
+            packet_id,
+            packet_length,
+            raw_buffer,
+        ):
+            self.add_packet(
+                protocol,
+                src,
+                dst,
+                icmp_type,
+                icmp_code,
+                icmp_seq,
+                ttl,
+                packet_id,
+                packet_length,
+                raw_buffer,
             )
-        else:
-            print("\033[92m[+] %s: TCP 0.0.0.0:%d->%s:%d\033[0m" %
-                (time.strftime("%H:%M:%S"), port, args.ip, port)
-            )
-    reactor.run()
 
-if __name__ == '__main__':
-    main()
+        # Run sniffing in a separate thread
+        import threading
+
+        self.sniffing_thread = threading.Thread(target=sniff, args=(host, callback), daemon=True)
+        self.sniffing_thread.start()
+        QMessageBox.information(
+            self, "Sniffing Started", f"Started sniffing on host: {host}"
+        )
+
+    def stop_sniffing(self):
+        if not self.sniffing_active:
+            QMessageBox.warning(self, "Warning", "Sniffing is not running.")
+            return
+
+        self.sniffing_active = False
+        QMessageBox.information(self, "Sniffing Stopped", "Packet sniffing has been stopped.")
+
+    def add_packet(
+        self,
+        protocol,
+        src,
+        dst,
+        icmp_type,
+        icmp_code,
+        icmp_seq,
+        ttl,
+        packet_id,
+        packet_length,
+        raw_buffer,
+    ):
+        row_position = self.table.rowCount()
+        self.table.insertRow(row_position)
+        self.table.setItem(row_position, 0, QTableWidgetItem(str(protocol)))
+        self.table.setItem(row_position, 1, QTableWidgetItem(str(src)))
+        self.table.setItem(row_position, 2, QTableWidgetItem(str(dst)))
+        self.table.setItem(row_position, 3, QTableWidgetItem(str(icmp_type)))
+        self.table.setItem(row_position, 4, QTableWidgetItem(str(icmp_code)))
+        self.table.setItem(row_position, 5, QTableWidgetItem(str(icmp_seq)))
+        self.table.setItem(row_position, 6, QTableWidgetItem(str(ttl)))
+        self.table.setItem(row_position, 7, QTableWidgetItem(str(packet_id)))
+        self.table.setItem(row_position, 8, QTableWidgetItem(str(packet_length)))
+
+        # Store raw packet data for detailed view
+        self.packet_data.append(
+            (protocol, src, dst, icmp_type, icmp_code, ttl, packet_id, raw_buffer)
+        )
+
+    def show_packet_details(self, row, column):
+        # Get the raw packet data for the selected row
+        packet_info = self.packet_data[row]
+
+        protocol, src, dst, icmp_type, icmp_code, ttl, packet_id, raw_buffer = (
+            packet_info
+        )
+
+        # Open the Packet Details Window
+        details_window = PacketDetailsWindow(
+            protocol, src, dst, icmp_type, icmp_code, ttl, packet_id, raw_buffer
+        )
+        details_window.exec_()
+
+    def toggle_column(self, col, state):
+        self.table.setColumnHidden(col, state == 0)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Packet Sniffer with Optional GUI")
+    parser.add_argument(
+        "-A", action="store_true", help="Launch the graphical environment"
+    )
+    parser.add_argument(
+        "-host", type=str, help="Specify the host IP address", default=""
+    )
+    parser.add_argument(
+        "-details", action="store_true", help="View detailed packet information in the CLI"
+    )
+    args = parser.parse_args()
+
+    if args.A:
+        app = QApplication(sys.argv)
+        main_window = PacketSnifferApp(default_host=args.host)
+        main_window.show()
+        sys.exit(app.exec_())
+    elif args.host:
+        def callback(
+            protocol,
+            src,
+            dst,
+            icmp_type,
+            icmp_code,
+            icmp_seq,
+            ttl,
+            packet_id,
+            packet_length,
+            raw_buffer,
+        ):
+            if args.details:
+                # Detailed CLI output
+                print("\n[Packet Captured]")
+                print(f"Protocol: {protocol}")
+                print(f"Source: {src}")
+                print(f"Destination: {dst}")
+                print(f"ICMP Type: {icmp_type}")
+                print(f"ICMP Code: {icmp_code}")
+                print(f"ICMP Seq: {icmp_seq}")
+                print(f"TTL: {ttl}")
+                print(f"Packet ID: {packet_id}")
+                print(f"Length: {packet_length}")
+                print(f"Raw Buffer: {binascii.hexlify(raw_buffer).decode()}\n")
+                print("-" * 60)
+            else:
+                # Basic CLI output
+                print(
+                    f"Protocol: {protocol}, Source: {src}, Destination: {dst}, ICMP Type: {icmp_type}, "
+                    f"ICMP Code: {icmp_code}, ICMP Seq: {icmp_seq}, TTL: {ttl}, ID: {packet_id}, Length: {packet_length}"
+                )
+
+        sniff(args.host, callback)
+    else:
+        print(
+            "Please use the -A flag to launch the graphical interface, -cli for detailed CLI output, "
+            "or specify a host with -host."
+        )
